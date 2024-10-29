e90dd4aa08e9d555bbda29dbd38d005e0cd3394b
Aron Cosmo Camacho <aron.cosmo@yahoo.es>
2024-10-29 15:46:59 +0100

    feat: add menu --array

--- src/main/java/org/TBMA/Main.java
+++ src/main/java/org/TBMA/Main.java
@@ -1,7 +1,15 @@
-
+package org.TBMA;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    String[] menu = {"Gestor de Libros Técnicos de Programación", "Ver todos los libros", "Añadir libro", "Eliminar libro", "Cambiar repositorio", "Salir"};
+
+
+    byte option
+    }
 
     }
 }
