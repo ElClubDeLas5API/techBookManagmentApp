88888d4e1b4f54d380e477b1f7e9cf83f7bcddd0
Carlassanchez24 <carlassanchez24@gmail.com>
2024-10-30 08:52:02 +0100

    feat: add menu completed

--- src/main/java/org/TBMA/Main.java
+++ src/main/java/org/TBMA/Main.java
@@ -1,15 +1,41 @@
 package org.TBMA;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] menu = {
+                "1. Añadir libro",
+                "2. Ver todos los libros",
+                "3. Eliminar libro",
+                "4. Cambiar repositorio",
+                "5. Salir"
+        };
 
-    String[] menu = {"Gestor de Libros Técnicos de Programación", "Ver todos los libros", "Añadir libro", "Eliminar libro", "Cambiar repositorio", "Salir"};
+        byte option = 0;
 
+        while (option != 5) {
+            for (String item : menu) {
+                System.out.println(item);
+            }
+            System.out.println("Seleccione una opción:");
 
-    byte option
-    }
+            String input = scanner.nextLine();
 
+            try {
+                option = Byte.parseByte(input);
+            } catch (Exception ex) {
+                System.out.println("Esto no es un número");
+                continue;
+            }
+
+            if (option < 1 || option > 5) {
+                System.out.println("Número fuera de rango");
+                continue;
+            }
+
+            System.out.println("Ha elegido: " + menu[option - 1]);
+        }
     }
 }
